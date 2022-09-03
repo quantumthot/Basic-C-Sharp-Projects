8c707a88511077539ae175e64a9bb45a2d4fd459
quantumthot <104961668+quantumthot@users.noreply.github.com>
2022-09-02 18:25:46 -0700

    Add files via upload
    calling methods from another file

---
+++ CallingMethods/CallingMethods/Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CallingMethods
+{
+    public class Math
+    {
+        public int Add(int inputNumber) // Add method will take in inputNumber from user
+        { 
+            int addition = (inputNumber + inputNumber);
+            return addition;
+        }
+
+        public int Divide(int inputNumber) //Constructor
+        {
+            int divide = (inputNumber / inputNumber);
+            return divide;
+        }
+
+
+        public int Square(int inputNumber) //Constructor
+        {
+            int squared = (inputNumber * inputNumber);
+            return squared;
+           
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("User, please enter a number you want to perform a math function with..");
+        int userInput = Convert.ToInt32(Console.ReadLine());
+
+        Math mymath = new Math(); // This calls in the class "Math" and naming the var mymath
+
+        int addition = mymath.Add(userInput); // Calls in the Add method
+        Console.WriteLine("Your number added by itself equals: " + addition);
+
+        int divide = mymath.Divide(userInput); // Calls in the divide method
+        Console.WriteLine("Your number divided by itself equals: " + divide);
+
+        int squared = mymath.Square(userInput); // Calls in the square method
+        Console.WriteLine("Your number squared by itself equals: " + squared);
+        Console.ReadLine();
+        }
+    }
+}
