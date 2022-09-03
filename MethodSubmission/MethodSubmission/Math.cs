9653ab29bb17ccf4d731fc31e01b7e23210fb816
quantumthot <104961668+quantumthot@users.noreply.github.com>
2022-09-03 00:44:22 -0700

    Add files via upload
    calling methods with if else

---
+++ MethodSubmission/MethodSubmission/Math.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodSubmission
+{
+    public class Math
+    {
+        public int Add(int inputNumber1, int inputNumber2) // Add method will take in inputNumber from user
+        {
+            int addition = (inputNumber1 + inputNumber2);
+            return addition;
+        }
+    }
+}
---
+++ MethodSubmission/MethodSubmission/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math mymath = new Math(); // This calls in the class "Math" and naming the var mymath
+
+
+            Console.WriteLine("User, please enter a number you want to perform a math function with..");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("User, do you want to enter a second number to perform a math function with? Enter \"y\" or \"n\" ?..");
+            string answer = Console.ReadLine();
+
+            if (answer == "y")
+            {
+                Console.WriteLine("User, please enter a second number you want to perform a math function with..");
+                int userInput2 = Convert.ToInt32(Console.ReadLine());
+                int addition = mymath.Add(userInput1, userInput2); // Calls in the Add method
+                Console.WriteLine("{0} added by {1} equals: " + addition, userInput1, userInput2);
+            }
+            else
+            {
+                int userInput3 = 0;
+                int addition = mymath.Add(userInput1, userInput3);
+                Console.WriteLine("{0} added to {1} equals: " + addition, userInput1, userInput3);
+            }
+        }
+    }
+}
