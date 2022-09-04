eede2476d0c2e2cfbbe096aaba1c4c8d78348fe8
quantumthot <104961668+quantumthot@users.noreply.github.com>
2022-09-03 23:44:10 -0700

    Add files via upload

---
+++ MethodClass/MethodClass/Math.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodClass
+{
+    class Math
+    {
+        public int Add(int userInput1, int userInput2) // Add method will take in inputNumber from user
+        {
+            if (userInput1 >= 0 )
+            {
+                int addition = userInput1 + userInput1;
+                return addition;
+            }
+            else
+            {
+                int showNum = userInput2;
+                return showNum;
+            }
+          
+            
+        }
+    }
+}
