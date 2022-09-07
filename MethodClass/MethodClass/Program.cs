576a17b42f99c63f6e22cb4ec1c3cb3fa3b18b25
quantumthot <104961668+quantumthot@users.noreply.github.com>
2022-09-06 22:04:29 -0700

    Add files via upload

--- MethodClass/MethodClass/Program.cs
+++ MethodClass/MethodClass/Program.cs
@@ -6,19 +6,19 @@ namespace MethodClass
     {
         static void Main(string[] args)
         {
+            Math add = new Math(); // Instantiating class
+            add.Sum(6, 4);
+        }
+    };
 
-            Math mymath = new Math(); // This calls in the class "Math" and naming the var mymath
-            
-           
-            Console.WriteLine("User, please enter a number you want to perform a math function with..");
-            int userInput1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("User, please enter another number..");
-            int userInput2 = Convert.ToInt32(Console.ReadLine());
-
-            int addition = mymath.Add(userInput1, userInput2); // Calls in the Add method   
-
-            Console.WriteLine("Your number added by itself equals: " + addition + " and the number entered is:  " + userInput2);
+    class Math
+    {
+        public void Sum(int a, int b) // Add method will take in inputNumber from user
+        {
+            int adding = a + a; 
+            Console.WriteLine("Here's a number: " + b);
+            Console.ReadLine();
         }
     }
 }
+
