d085440f4b807c00c7364789cba4f215c5d7331d
quantumthot <104961668+quantumthot@users.noreply.github.com>
2022-09-06 22:22:10 -0700

    Add files via upload

---
+++ ClassSubmission/ClassSubmission/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User, please enter a number.");
+            int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+            Math output = new Math();
+            output.IntOut(inputNumber);
+        }
+    };
+    class Math
+    {
+        public void IntOut(int inputNumber)
+        {
+            int sum = (inputNumber / 2);
+            Console.WriteLine("Your number divided by 2 is: " + sum);
+            Console.ReadLine();
+        }
+    }
+
+}
