3bc962fd906877fcf5765f8a1a82034826c80843
quantumthot <104961668+quantumthot@users.noreply.github.com>
2022-09-09 14:26:20 -0700

    Add files via upload

---
+++ OperatorsSubmission/OperatorsSubmission/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace OperatorsSubmission
+{
+    class Employee
+    {
+        int id;
+        string FirstName;
+        string LastName;
+
+
+        public Employee(int id, string FirstName, string LastName)
+        {
+            this.id = id;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("First Name : " + FirstName);
+            Console.WriteLine("Last Name: " + LastName);
+        }
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+                if((employee1.id == employee2.id))
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            if ((employee1.id != employee2.id))
+            {
+                return true;
+            }
+            return false;
+        }  
+    }
+}
---
+++ OperatorsSubmission/OperatorsSubmission/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OperatorsSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee(1, "Karl", "Malone");
+            employee1.Display();
+
+            Employee employee2 = new Employee(2, "John", "Stockton");
+            employee2.Display();
+
+            bool v = employee2 == employee1;
+            bool w = employee2 != employee1;
+            Console.WriteLine("Employee1 and Employee2's Ids match: " + v);
+            Console.WriteLine("Employee1 and Employee2's Ids do not match: " + w);
+            Console.ReadLine();
+        }
+    }
+}
