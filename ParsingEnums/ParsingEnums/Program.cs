e72e4771ba55cb62428324ec9eaa65c39b7327b1
quantumthot <104961668+quantumthot@users.noreply.github.com>
2022-09-10 17:21:20 -0700

    Add files via upload

---
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ParsingEnums
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("User, please enter the current day of the week");
+                DaysofTheWeek pickDay = (DaysofTheWeek)Enum.Parse(typeof(DaysofTheWeek), Console.ReadLine(), true);
+                Console.WriteLine("The day you picked was {0}", pickDay);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual numerical day of the week.", ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+        public enum DaysofTheWeek
+        {    
+            Monday = 0,
+            Tuesday = 1,
+            Wednesday = 2,
+            Thursday =3,
+            Friday = 4,
+            Saturday = 5,
+            Sunday = 6
+        }
+    }
+}
