f4f507696475748f6ef22168cd507bbec4a5cb56
quantumthot <104961668+quantumthot@users.noreply.github.com>
2022-09-12 17:40:55 -0700

    Add files via upload

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Current Date and Time is : ");
+            DateTime now = DateTime.Now; // Creating new var that pulls in current time and date
+            Console.WriteLine(now);
+            Console.WriteLine("User, please enter a number");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            DateTime x = now.AddHours(userNumber); // Creating new var that adds userNumber to current time.
+            Console.WriteLine("The time it will be in {0} hours will be {1}", userNumber, x);
+            Console.ReadLine();
+        }
+    }
+}
