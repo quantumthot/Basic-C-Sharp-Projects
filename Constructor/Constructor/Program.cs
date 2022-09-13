a74caef624ee5e702a1d554efde65ddda8cfac31
quantumthot <104961668+quantumthot@users.noreply.github.com>
2022-09-13 14:56:11 -0700

    Add files via upload

--- Constructor/Constructor/Program.cs
+++ Constructor/Constructor/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Constructor
 {
@@ -6,18 +10,18 @@ namespace Constructor
     {
         static void Main(string[] args)
         {
-            var user = new User("Tyler");
-            const string location = "Zoo";
+            var user = new User("Tyler"); 
+            const string location = "Zoo"; // Creating a const string
             Console.WriteLine("{0} {1}", user, location);
         }           
     }
     public class User
     {
-        public User(string username) : this (username, "Home")
+        public User(string username) : this (username, "Home") //second constructor pulling in from constructor below.
         {
         }
 
-        public User(string username, string location)
+        public User(string username, string location) // original constructor
         {
             this.username = username;
             this.location = location;
