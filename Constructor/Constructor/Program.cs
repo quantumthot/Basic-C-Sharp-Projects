3886905f5445ce711bcbba30364e8d7f28e8bad5
quantumthot <104961668+quantumthot@users.noreply.github.com>
2022-09-13 14:21:39 -0700

    Add files via upload

---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Constructor
+    {
+        static void Main(string[] args)
+        {
+            var user = new User("Tyler");
+            const string location = "Zoo";
+            Console.WriteLine("{0} {1}", user, location);
+        }           
+    }
+    public class User
+    {
+        public User(string username) : this (username, "Home")
+        {
+        }
+
+        public User(string username, string location)
+        {
+            this.username = username;
+            this.location = location;
+        }
+
+        public string username { get; set; }
+        public string location { get; set; }
+    }
+}
