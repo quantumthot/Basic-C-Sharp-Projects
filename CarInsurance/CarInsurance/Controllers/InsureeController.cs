c86ddf037157db332734e49cc0b56bce3f000f14
quantumthot <104961668+quantumthot@users.noreply.github.com>
2022-09-14 14:00:43 -0700

    Car Insurance MVC
    Car Insurance Project with MVC

---
+++ CarInsurance/CarInsurance/Controllers/InsureeController.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarInsurance.Models;
+
+namespace CarInsurance.Controllers
+{
+    public class InsureeController : Controller
+    {
+        private InsuranceEntities db = new InsuranceEntities();
+
+        public ActionResult Admin()
+        {
+            return View(db.Tables.ToList());
+        }
+
+        // GET: Insuree
+        public ActionResult Index()
+        {
+            return View(db.Tables.ToList());
+        }
+
+        // GET: Insuree/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // GET: Insuree/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Insuree/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Table table)
+        {
+            table.Quote = 50;
+
+            if ((DateTime.Now.Year - table.DateOfBirth.Year) <= 18) // if user is 18 or younger, they get 100 added on to quote
+            {
+                table.Quote += 100;
+            }
+            else if ((DateTime.Now.Year - table.DateOfBirth.Year) < 25 && (DateTime.Now.Year - table.DateOfBirth.Year) > 18)
+            {
+                table.Quote += 50;
+            }
+            else if ((DateTime.Now.Year - table.DateOfBirth.Year) >= 26)
+            {
+                table.Quote += 25;
+            }
+
+
+            if (table.CarYear < 2000)
+            {
+                table.Quote += 25;
+            }
+            else if (table.CarYear > 2015)
+            {
+                table.Quote += 25;
+            }
+
+            if (table.CarMake == "Porsche" || table.CarMake == "porsche" && table.CarModel == "911 Carrera" || table.CarModel == "911 carrera")
+            {
+                table.Quote += 50;
+            }
+            else if (table.CarMake == "Porsche" || table.CarMake == "porsche")
+            {
+                table.Quote += 25;
+            }
+
+            if (table.SpeedingTickets > 0)
+            {
+                table.Quote += (table.SpeedingTickets * 10);
+            }
+
+            if (table.DUI == true)
+            {
+                table.Quote *= 1.25m;
+            }
+
+            if (table.CoverageType == true)
+            {
+                table.Quote *= 1.50m;
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.Tables.Add(table);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(table);
+        }
+
+        // GET: Insuree/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // POST: Insuree/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(table).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(table);
+        }
+
+        // GET: Insuree/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // POST: Insuree/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Table table = db.Tables.Find(id);
+            db.Tables.Remove(table);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
